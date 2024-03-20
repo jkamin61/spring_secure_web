49732b1b1a53b6b08b804082178c7964aab0545c
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-03-20 22:48:00 +0100

    -

---
+++ src/test/java/com/example/spring_secure_app/SpringSecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_secure_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
